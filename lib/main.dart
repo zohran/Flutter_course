ffa951f0b9726bb10a1784920cf0d2fd4571db9c
Zohran <zeerasheed97@gmail.com>
2021-06-25 16:44:02 +0500

    Day2 Completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_application_1/home_page.dart';
+import 'package:flutter_application_1/Screens/Login.dart';
+import 'package:flutter_application_1/Screens/home_page.dart';
 
 void main(List<String> args) {
   runApp(Zohran());
@@ -11,7 +12,21 @@ class Zohran extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Homepage(),
+      //home: Homepage(),
+      themeMode: ThemeMode.light,
+      theme: ThemeData(
+        primarySwatch: Colors.red,
+      ),
+      darkTheme: ThemeData(
+        brightness: Brightness.dark,
+      ),
+      initialRoute: "/home",
+      routes: {
+        //"/": (context) => Homepage(),
+        "/home": (context) => Homepage(),
+        "/LoginPage": (context) => LoginPage(),
+      },
+      //1home: Homepage(),
     );
   }
 }
