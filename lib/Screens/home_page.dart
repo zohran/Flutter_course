4d0eb39fa6a134fa38f03990badff236c2378a69
Zohran <zeerasheed97@gmail.com>
2021-06-28 17:54:41 +0500

    Day11 completed

--- lib/Screens/home_page.dart
+++ lib/Screens/home_page.dart
@@ -1,14 +1,37 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_application_1/models/catalog.dart';
 import 'package:flutter_application_1/widgets/ItemWidget.dart';
 import 'package:flutter_application_1/widgets/drawer.dart';
+import 'dart:convert';
 
-class Homepage extends StatelessWidget {
+class Homepage extends StatefulWidget {
   const Homepage({Key? key}) : super(key: key);
 
   @override
+  _HomepageState createState() => _HomepageState();
+}
+
+class _HomepageState extends State<Homepage> {
+  @override
+  void initState() {
+    super.initState();
+    loadData();
+  }
+
+  loadData() async {
+    var prod = await rootBundle.loadString("assets/files/products.json");
+    // ignore: non_constant_identifier_names
+    var deco_prod = jsonDecode(prod);
+    var dataprod = deco_prod["products"];
+    CatalogModel.items =
+        List.from(dataprod).map<Item>((item) => Item.fromMap(item)).toList();
+    setState(() {});
+  }
+
+  @override
   Widget build(BuildContext context) {
-    final MeriList = List.generate(20, (index) => CatalogModel.items[0]);
+    // ignore: non_constant_identifier_names
     return Container(
       child: Scaffold(
         appBar: AppBar(
@@ -17,10 +40,10 @@ class Homepage extends StatelessWidget {
         body: Padding(
           padding: const EdgeInsets.all(20.0),
           child: ListView.builder(
-            itemCount: MeriList.length,
+            itemCount: CatalogModel.items.length,
             itemBuilder: (context, index) {
               return ItemWidget(
-                item: MeriList[index],
+                item: CatalogModel.items[index],
               );
             },
           ),
